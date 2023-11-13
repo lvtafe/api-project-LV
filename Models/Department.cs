d34e101fe97bdfc595929bbf06204d6e600de466
Lisa <lisa.viljoen@studytafensw.edu.au>
2023-11-13 11:57:06 +1100

    initial api

---
+++ Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace api_project_LV.Models;
+
+public partial class Department
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Person> People { get; set; } = new List<Person>();
+}
